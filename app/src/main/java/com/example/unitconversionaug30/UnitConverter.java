0a8f8baeee64c71b7897d730f9ace27c756fc55b
Akash <dilliwalakash@gmail.com>
2019-09-05 14:16:57 -0400

    commit 2.4

--- app/src/main/java/com/example/unitconversionaug30/UnitConverter.java
+++ app/src/main/java/com/example/unitconversionaug30/UnitConverter.java
@@ -3,6 +3,8 @@ package com.example.unitconversionaug30;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,10 +12,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UnitConverter extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -21,6 +27,7 @@ public class UnitConverter extends AppCompatActivity implements AdapterView.OnIt
     Spinner spinnerList;
     Spinner spinnerUnits, spinnerSecondUnits;
     EditText editTextValue;
+    TextView valueTv;
     Button button;
     List<String> paths = new ArrayList<String>();
     List<String> units = new ArrayList<String>();
@@ -35,16 +42,12 @@ public class UnitConverter extends AppCompatActivity implements AdapterView.OnIt
 
         spinnerList = (Spinner) findViewById(R.id.spinnerListMeasurement);
         spinnerList.setOnItemSelectedListener(this);
+
        spinnerUnits =  findViewById(R.id.spinnerListUnits);
        spinnerSecondUnits = findViewById(R.id.spinnerSecondUnit);
        editTextValue = findViewById(R.id.editText);
+       valueTv = findViewById(R.id.valueTv);
        button = findViewById(R.id.button);
-        //
-
-
-       // spinnerList.setAdapter(arrayAdapter);
-
-       // Toast.makeText(this,spinnerList.toString(),Toast.LENGTH_LONG).show();
 
 
 
@@ -56,8 +59,7 @@ public class UnitConverter extends AppCompatActivity implements AdapterView.OnIt
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
 
-
-        Toast.makeText(this,"selected "+i,Toast.LENGTH_LONG).show();
+        Log.d("selected", "onItemSelected: " +adapterView.getId());
 
         if(i==0){
             state = new String[]{"Kilometer","Miles"};
@@ -69,11 +71,21 @@ public class UnitConverter extends AppCompatActivity implements AdapterView.OnIt
 
         }
 
+        if(i==2){
+
+            state = new String[]{"Celsius","Fahrenheit"};
+
+
+        }
+
 
         ArrayAdapter <String> arrayAdapter = new ArrayAdapter<String>(UnitConverter.this,R.layout.support_simple_spinner_dropdown_item, state);
 
        spinnerUnits.setAdapter(arrayAdapter);
+
        spinnerSecondUnits.setAdapter(arrayAdapter);
+
+
 
 
 
@@ -89,22 +101,63 @@ public class UnitConverter extends AppCompatActivity implements AdapterView.OnIt
 
     public void convert(View view) {
 
-        int value= 0;
-        String unit1 = spinnerUnits.getSelectedItem().toString();
-        String unit2 = spinnerSecondUnits.getSelectedItem().toString();
-        value = Integer.parseInt(editTextValue.getText().toString());
-        if(unit1.equals("Kilograms") && unit2.equals("Pounds")){
+
+        if (editTextValue.getText().toString().isEmpty()) {
+            valueTv.setText("");
+
+            return;
 
 
+        } else {
 
-            Toast.makeText(this,"Value in Pounds is: "+value*2.05,Toast.LENGTH_LONG).show();
 
+            DecimalFormat f = new DecimalFormat("##.00");
+            int value = 0;
+            String unit1 = spinnerUnits.getSelectedItem().toString();
+            String unit2 = spinnerSecondUnits.getSelectedItem().toString();
+            value = Integer.parseInt(editTextValue.getText().toString());
+
+
+            if (unit1.equals("Kilograms") && unit2.equals("Pounds")) {
+
+                valueTv.setText("" + f.format(value * 2.05));
+
+
+                Toast.makeText(this, "Value in Pounds is: " + value * 2.05, Toast.LENGTH_LONG).show();
+            }
+            if (unit1.equals("Kilometer") && unit2.equals("Miles")) {
+
+                valueTv.setText("" + f.format(value / 1.609));
+                //valueTv.setFilters(new InputFilter[] {new DecimalDigitsInputFilter(5,2)});
+
+            }
+
+            if(unit1.equals("Pounds") && unit2.equals("Kilograms")){
+                valueTv.setText("" + f.format(value / 2.05));
+
+            }
+
+            if (unit1.equals("Miles") && unit2.equals("Kilometer")) {
+
+                valueTv.setText("" + f.format(value * 1.609));
+                //valueTv.setFilters(new InputFilter[] {new DecimalDigitsInputFilter(5,2)});
+
+            }
+            if (unit1.equals("Fahrenheit") && unit2.equals("Celsius")) {
+
+                valueTv.setText("" + f.format((value-32)*5/9));
+                //valueTv.setFilters(new InputFilter[] {new DecimalDigitsInputFilter(5,2)});
+
+            }
+            if (unit1.equals("Celsius") && unit2.equals("Fahrenheit")) {
+
+                valueTv.setText("" + f.format((value*9/5)+32));
+                //valueTv.setFilters(new InputFilter[] {new DecimalDigitsInputFilter(5,2)});
+
+            }
 
 
         }
-
-
-
 
     }
 }
